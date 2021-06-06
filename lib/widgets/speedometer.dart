a0d5de8f1c12b7e4afe86d439ba393a925f90b5a
jerry <jerrytiapalmiotto@gmail.com>
2021-06-06 16:17:20 +0200

    💩 Implemented tachometer arrow (WIP)

--- lib/widgets/speedometer.dart
+++ lib/widgets/speedometer.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:ae86_speedometer/tasks/play_chime_background_task.dart';
 import 'package:audio_service/audio_service.dart';
@@ -66,14 +68,57 @@ class _SpeedometerState extends State<Speedometer> {
                   left: double.parse(
                       tachometerConfig.get('Speed Unit', 'unit_x') ?? '0'),
                 ),
+                // Codice per visualizzare la freccia del tachimetro
+                Transform.rotate(
+                  angle: 0,
+                  origin: Offset(
+                    double.parse(
+                        tachometerConfig.get('RPM Bar', 'rpm_center_x') ?? '0'
+                    ),
+                    double.parse(
+                        tachometerConfig.get('RPM Bar', 'rpm_center_y') ?? '0'
+                    ),
+                  ),
+                  alignment: Alignment.topLeft,
+                  child: Positioned(
+                    child: Transform(
+                      transform: Matrix4.translationValues(
+                          double.parse(
+                              tachometerConfig.get('RPM Bar', 'rpm_x') ?? '0'
+                          ),
+                          double.parse(
+                              tachometerConfig.get('RPM Bar', 'rpm_y') ?? '0'
+                          ),
+                          0
+                      ),
+                      origin: Offset(
+                        double.parse(
+                            tachometerConfig.get('RPM Bar', 'rpm_center_x') ?? '0'
+                        ),
+                        double.parse(
+                            tachometerConfig.get('RPM Bar', 'rpm_center_y') ?? '0'
+                        ),
+                      ),
+                      child: Image.asset(
+                        "assets/tachometers/$loadedTachometerTheme/rpm_bar/rpm_bar.png",
+                        width: double.parse(
+                            tachometerConfig.get('RPM Bar', 'rpm_width') ?? '0'),
+                        height: double.parse(
+                            tachometerConfig.get('RPM Bar', 'rpm_height') ?? '0'),
+                      ),
+                    ),
+                  ),
+                ),
                 // Etichette degli RPM
                 Positioned(
                   child: Image.asset(
                     "assets/tachometers/$loadedTachometerTheme/background/labels_20k.png",
                     width: double.parse(
-                        tachometerConfig.get('RPM Gauge', 'gauge_width') ?? '0'),
+                        tachometerConfig.get('RPM Gauge', 'gauge_width') ??
+                            '0'),
                     height: double.parse(
-                        tachometerConfig.get('RPM Gauge', 'gauge_height') ?? '0'),
+                        tachometerConfig.get('RPM Gauge', 'gauge_height') ??
+                            '0'),
                   ),
                   top: double.parse(
                       tachometerConfig.get('RPM Gauge', 'gauge_y') ?? '0'),
@@ -81,8 +126,15 @@ class _SpeedometerState extends State<Speedometer> {
                       tachometerConfig.get('RPM Gauge', 'gauge_x') ?? '0'),
                 ),
                 // Velocità in Km/h
-                ...showSpeed(widget.speed)
+                ...showSpeed(widget.speed),
                 // TOOD: cercare di capire come aggiungere la lancetta basata sui Km/h
+                /*Positioned(
+                  child:
+                  top: double.parse(
+                      tachometerConfig.get('RPM Bar', 'rpm_center_y') ?? '0'),
+                  left: double.parse(
+                      tachometerConfig.get('RPM Bar', 'rpm_center_y') ?? '0'),
+                )*/
               ],
             ),
           )
@@ -94,32 +146,26 @@ class _SpeedometerState extends State<Speedometer> {
   List<Widget> showSpeed(double speed) {
     String roundedSpeed = (speed.toInt()).toString();
     List<Widget> widgets = [];
-    double speedDigitWidth = double.parse(
-        tachometerConfig.get('Speed', 'speed_width') ?? '0'
-    );
-    double speedDigitHeight = double.parse(
-    tachometerConfig.get('Speed', 'speed_height') ?? '0'
-    );
-    double speedDigitY = double.parse(
-        tachometerConfig.get('Speed', 'speed_y') ?? '0'
-    );
-    double speedDigitX = double.parse(
-        tachometerConfig.get('Speed', 'speed_x') ?? '0'
-    );
+    double speedDigitWidth =
+        double.parse(tachometerConfig.get('Speed', 'speed_width') ?? '0');
+    double speedDigitHeight =
+        double.parse(tachometerConfig.get('Speed', 'speed_height') ?? '0');
+    double speedDigitY =
+        double.parse(tachometerConfig.get('Speed', 'speed_y') ?? '0');
+    double speedDigitX =
+        double.parse(tachometerConfig.get('Speed', 'speed_x') ?? '0');
 
     for (int i = 0; i < roundedSpeed.length; i++) {
-      widgets.add(
-        Positioned(
-            child: Image.asset(
-              "assets/tachometers/$loadedTachometerTheme/speed_yellow/speed_digits_${roundedSpeed[i]}.png",
-              width: speedDigitWidth,
-              height: speedDigitHeight,
-            ),
-            top: speedDigitY,
-            // x - (width * i)
-            left: speedDigitX - (speedDigitWidth * (roundedSpeed.length - (i + 1)))
-        )
-      );
+      widgets.add(Positioned(
+          child: Image.asset(
+            "assets/tachometers/$loadedTachometerTheme/speed_yellow/speed_digits_${roundedSpeed[i]}.png",
+            width: speedDigitWidth,
+            height: speedDigitHeight,
+          ),
+          top: speedDigitY,
+          // x - (width * i)
+          left: speedDigitX -
+              (speedDigitWidth * (roundedSpeed.length - (i + 1)))));
     }
 
     return widgets;
